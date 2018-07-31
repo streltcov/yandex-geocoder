c15bdfe7a8ad68f4f305d51aef3f2e404ab8de35
Петр Стрельцов <streltsov83@yandex.ru>
2018-07-31 23:28:05 +0300

    license added

--- src/GeoData.php
+++ src/GeoData.php
@@ -3,18 +3,31 @@
 namespace streltcov\geocoder;
 
 use streltcov\geocoder\interfaces\GeoDataInterface;
-use streltcov\YandexGeocoder\GeoCoder;
+
+/**
+ * Copyright 2018 Peter Streltsov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
 
 /**
  * Class GeoData
  * Contains Yandex geocoder response data
  *
- * @package app\models\yandexgeo
+ * @package streltcov\yandex-geocoder
  */
 class GeoData implements GeoDataInterface
 {
 
-    //private $address;
     private $geocoderMetaData;
     private $metaDataProperty;
     private $featureMember;
